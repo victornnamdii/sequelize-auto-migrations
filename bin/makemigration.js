7728a7eb777050cd95c3742fddc5df1cda9b0187
“victornnamdii” <victorilodiuba@gmail.com>
2025-04-08 13:44:43 +0100

    chore: use 2 digit date for naming

--- bin/makemigration.js
+++ bin/makemigration.js
@@ -69,7 +69,7 @@ const getDateTime = () => {
 
   const year = now.getFullYear();
   const month = (now.getMonth() + 1).toString().padStart(2, "0");
-  const date = now.getDate();
+  const date = now.getDate().toString().padStart(2, "0");
 
   const hour = now.getHours();
   const minute = now.getMinutes();
